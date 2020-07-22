3b9eb26afbc6711ad7b3179506c41b9dae327bde
JeremyAndress <jeremy.a.silva.silva@gmail.com>
2020-07-21 20:19:46 -0400

    :package: Add alembic migrations

---
+++ src/alembic/versions/4524eb6a5e88_add_department_employee_link.py
@@ -0,0 +1,24 @@
+"""add department_employee_link
+
+Revision ID: 4524eb6a5e88
+Revises: 79fa6422aab8
+Create Date: 2020-07-21 20:09:28.351684
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4524eb6a5e88'
+down_revision = '79fa6422aab8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ src/alembic/versions/79fa6422aab8_add_department_employee_link.py
@@ -0,0 +1,24 @@
+"""add department_employee_link
+
+Revision ID: 79fa6422aab8
+Revises: 
+Create Date: 2020-07-21 19:50:09.874658
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '79fa6422aab8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
