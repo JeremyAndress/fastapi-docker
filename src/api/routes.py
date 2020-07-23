98a18b5fc1b226c5d49e1b33b4d754a4789f4637
JeremyAndress <jeremy.a.silva.silva@gmail.com>
2020-07-22 20:03:17 -0400

    :package: update fast api version

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
 router = APIRouter()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
 @router.get("/CAB/testing")
-def firts(id: int):
+def firts(id: int,token: str = Depends(oauth2_scheme)):
     return {
         "id": id,
         "status": 200
--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ app = FastAPI(
     title=settings.PROJECT_NAME, 
     openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
+
 app.include_router(
     router, prefix=settings.API_V1_STR, tags=["test"]
 )
