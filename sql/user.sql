5a308b55473223c36507ea7cd16f8c2d89b17437
JeremyAndress <jeremy.a.silva.silva@gmail.com>
2020-10-26 16:34:41 -0300

    ADD: username 25 lenght and unique

--- sql/user.sql
+++ sql/user.sql
@@ -1,8 +1,11 @@
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) NOT NULL,
+  `username` varchar(25) NOT NULL,
   `password` varchar(100) NOT NULL,
+  `rol_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `user_UN` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+  UNIQUE KEY `user_UN` (`username`),
+  KEY `user_rol_FK` (`rol_id`),
+  CONSTRAINT `user_rol_FK` FOREIGN KEY (`rol_id`) REFERENCES `rol` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1
