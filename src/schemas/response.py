281fe02b7f9415603f6dff21ca0ec444fd5add4a
JeremyAndress <jeremy.a.silva.silva@gmail.com>
2020-11-18 20:50:19 -0300

    ADD: Reponse_WITH_ID schema

--- src/schemas/response.py
+++ src/schemas/response.py
@@ -5,6 +5,9 @@ class Response_SM(BaseModel):
     status: bool
     result: str
 
+class Reponse_WITH_ID(Response_SM):
+    id: int
+
 class Pagination(BaseModel):
     previous_page: Optional[int]
     next_page: Optional[int]
