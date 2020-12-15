77859814054745274dba0ce189accad13a677934
JeremyAndress <jeremysilvasilva@gmail.com>
2020-12-15 15:39:08 -0300

    :fire: Removing sql User and Role Files

--- sql/rol.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE `rol` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `rol_UN` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
