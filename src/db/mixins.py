984a1c7f02f2448d063aa41256020d60310ffeb9
JeremyAndress <jeremysilvasilva@gmail.com>
2021-02-16 13:23:16 -0300

    :lipstick: Fix mixins linter

--- src/db/mixins.py
+++ src/db/mixins.py
@@ -3,9 +3,9 @@ from sqlalchemy import Column
 from .uuid import UUIDType
 
 
-class MixinGetById:
+class MixinGetById(object):
     id = Column(UUIDType(binary=False), primary_key=True, default=v4)
 
     @classmethod
     def get_by_id(cls, id: v4):
-        return session.query(cls).filter(cls.id == id).first()
+        return cls.session.query(cls).filter(cls.id == id).first()
